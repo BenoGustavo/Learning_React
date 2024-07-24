9029a03cbacb56e3cfd080f05f5ce259737e91ce
Gustavo <gustavoleandrogorges@gmail.com>
2024-07-24 17:27:32 -0300

    feat: config app

---
+++ QuantoValeAFipe/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.module.scss"
+
+export const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>Header</h1>
+        </header>
+    );
+}
